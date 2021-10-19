50f92288bba5851f820be7ee1e332cd9e878e25d
Linux <ivan@pop-os.localdomain>
2021-10-19 19:40:15 -0300

    Requisito 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,4 +1,4 @@
 -- 10 - Mostre todos os dados da tabela `purchase_orders` em ordem decrescente, 
 -- ordenados por `created_by` em que o `created_by` é maior ou igual a 3.
 SELECT * FROM northwind.purchase_orders
-ORDER BY created_by DESC, created_by >= 3;
+WHERE created_by >= 3 order by created_by DESC;
